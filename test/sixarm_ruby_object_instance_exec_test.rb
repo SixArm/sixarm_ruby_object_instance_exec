e3f9b5f0d77be966dbd3ce2a00ba49a0a28e4c98
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:46 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_object_instance_exec_test.rb
+++ test/sixarm_ruby_object_instance_exec_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_object_instance_exec'
+require "sixarm_ruby_object_instance_exec"
 
 # Original source at http://eigenclass.org/hiki.rb?bounded+space+instance_exec
 class Testing < Test::Unit::TestCase
